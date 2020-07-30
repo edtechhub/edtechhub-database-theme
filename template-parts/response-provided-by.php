f6522de77a6f18016948e442d92c99c378050d16
Dafydd Vaughan <dai@daibach.co.uk>
2020-07-30 15:57:26 +0100

    Rework ‘provided by’ template based on feedback

--- template-parts/response-provided-by.php
+++ template-parts/response-provided-by.php
@@ -1,18 +1,20 @@
-<p><strong>Provided by:</strong>
-  <?php
-    $lead_organisation = get_post_meta($post->ID,'tool_developer',true);
-    $website = get_post_meta($post->ID,'website',true);
+<?php
+$lead_organisation = get_post_meta($post->ID,'tool_developer',true);
+$website = get_post_meta($post->ID,'website',true);
 
-    if($lead_organisation != '') :
-      if($website != '') : ?>
-        <a href="<?php echo $website ; ?>"><?php echo $lead_organisation; ?></a>
-        <br/><strong>Website:</strong> <a href="<?php echo $website ; ?>"><?php echo $website; ?></a>
-  <?php
-      else : ?>
-        <?php echo $lead_organisation; ?>
-  <?php
-      endif;
-    else : ?>
-      Unknown
-  <?php endif; ?>
-</p>
+if($lead_organisation != '' || $website != '') : ?><p><?php
+  if($lead_organisation != '') : ?>
+    <strong>Provided by:</strong> <a href="<?php echo $website ; ?>"><?php echo $lead_organisation; ?></a>
+    <?php
+  endif;
+
+  if($lead_organisation != '' && $website != '') : ?><br/><?php endif;
+
+  if($website != '') : ?>
+    <strong>Website:</strong> <a href="<?php echo $website ; ?>"><?php echo $website; ?></a>
+    <?php
+  endif;
+
+  ?></p><?php
+endif;
+?>
