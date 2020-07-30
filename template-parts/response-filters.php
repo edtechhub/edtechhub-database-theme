96b3de842698b1dbd4b202ce02e96b3dc3cf2780
Dafydd Vaughan <dai@daibach.co.uk>
2020-07-30 16:05:43 +0100

    Use `search-filter` plugin if it is switched on

--- template-parts/response-filters.php
+++ template-parts/response-filters.php
@@ -1,3 +1,9 @@
+<?php if(is_plugin_active('search-filter/search-filter.php')) : ?>
+
+  <?php echo do_shortcode( '[searchandfilter types="checkbox,checkbox,checkbox,checkbox,checkbox,checkbox" headings="Target users,Tool target levels,Tool approaches,Tool connectivity requirements,Terms,Open source" post_types="tools" fields="tool_target_user,tool_target_level,tool_approach,tool_connectivity_requirement,tool_terms_and_costs,tool_code_type"]' ); ?>
+
+<?php else : ?>
+
 <?php
   $term_slug = FALSE;
   if(is_tax()) {
@@ -41,3 +47,4 @@
   $filter_tax = "tool_code_type";
   include('response-filter-item.php');
 ?>
+<?php endif; ?>
